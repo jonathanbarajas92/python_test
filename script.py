f90126740eddc7d4153ce58767357ddc4c82666b
Jonathan Barajas Hernandez <jonathanbarajas92@gmail.com>
2026-07-10 01:12:16 -0600

    Added user print statements

---
+++ script.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+print("user 2")
+print("user 3")
+print("user 4")
